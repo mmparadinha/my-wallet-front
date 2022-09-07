d376ccece0e6c1d9b4e032cb66bd054b57ee14ba
mmparadinha <matheusmparadinha@gmail.com>
2022-09-07 16:32:25 -0300

    Add LoginPage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import React from "react";
+import GlobalStyles from "./components/GlobalStyles";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginPage from "./components/LoginPage";
+import SignUpPage from "./components/SignUpPage";
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/sign-up" element={<SignUpPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/components/LoginPage.js
@@ -0,0 +1,128 @@
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+
+function LoginPage() {
+    return (
+        <Main>
+            <h1>MyWallet</h1>
+
+            <Box /* onSubmit={entrar} */>
+                <Input
+                    // disabled={entrando}
+                    // required
+                    type='email'
+                    // name='email'
+                    // value={login.password}
+                    // onChange={atualizarInput}
+                    placeholder='E-mail'
+                />
+                <Input
+                    // disabled={entrando}
+                    // required
+                    type='password'
+                    // name='password'
+                    // value={login.password}
+                    // onChange={atualizarInput}
+                    placeholder='Senha'
+                />
+                <Button type='submit' /* disabled={entrando}>{entrando ? <Loading /> : 'Entrar'} */>Entrar</Button>
+            </Box>
+
+            <Link to="/sign-up"><h2>Primeira vez? Cadastre-se</h2></Link>
+        </Main>
+    );
+}
+
+export default LoginPage;
+
+const Main = styled.div`
+    background-color: purple;
+    min-height: 100vh;
+    margin: auto;
+    padding: 10%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    h1 {
+        font-family: 'Saira Stencil One', cursive;
+        font-weight: 400;
+        font-size: 32px;
+        color: #FFFFFF;
+        margin-bottom: 24px;
+    }
+
+    h2 {
+        font-family: 'Raleway', sans-serif;
+        font-weight: 700;
+        font-size: 15px;
+        color: #FFFFFF;
+        margin-top: 36px;
+        text-align: center;
+
+        &:hover {
+            text-decoration: underline;
+            cursor: pointer;
+        }
+
+        &:disabled {
+            opacity: 0.8;
+            cursor: default;
+        }
+    }
+`;
+
+const Box = styled.form`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 24px;
+`;
+
+const Input = styled.input`
+    width: 100%;
+    height: 58px;
+    font-family: 'Raleway', sans-serif;
+    font-weight: 400;
+    font-size: 20px;
+    color: #000000;
+    padding: 10px;
+    border-radius: 5px;
+
+    &::placeholder {
+        color: #000000;
+        opacity: 0.8;
+    }
+
+    &:disabled {
+        background-color: #F2F2F2;
+        color: #D4D4D4;
+    }
+`;
+
+const Button = styled.button`
+    background-color: #A328D6;
+    width: 100%;
+    height: 46px;
+    border: none;
+    border-radius: 5px;
+    font-family: 'Raleway', sans-serif;
+    font-weight: 700;
+    font-size: 20px;
+    color: #FFFFFF;
+    padding: 10px;
+
+    &:hover {
+        filter: brightness(1.2);
+        cursor: pointer;
+    }
+    
+    &:disabled {
+        filter: brightness(0.7);
+        cursor: default;
+    }
+`;
